eee2cc6731b15dcb9df102c8c0c4e5fe7ac09d9c
Cyril Savari Bosch J P <cyril.jp@techjays.com>
2025-04-25 13:04:43 +0000

    Add the image Around.png from src/app/techjays-overview/Around.png

--- src/app/techjays-overview/page.tsx
+++ src/app/techjays-overview/page.tsx
@@ -2,12 +2,26 @@
 
 import TechjaysOverview from '@/components/TechjaysOverview';
 import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+import AroundImage from './Around.png';
 
 export default function TechjaysOverviewPage() {
   return (
     <>
       <Navbar />
       <TechjaysOverview />
+       <Image
+          src={AroundImage}
+          alt="Techjays Overview"
+          width={1200}
+          height={800}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+          className="rounded-md object-cover"
+        />
     </>
   );
 }
+
