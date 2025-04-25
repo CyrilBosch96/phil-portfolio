3123238b91f75a2b23c0a68967063ada8038bcae
Cyril Savari Bosch J P <cyril.jp@techjays.com>
2025-04-25 12:54:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Navbar>
    
    with the text content:
    
        Philip Clements SamuelrajTechjays OverviewInvestment PortfolioMedia MentionsHobbiesContact
    
    Relevant files:
    
        - https://6000-idx-studio-1745582025608.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_1988d3ed._.js
        - https://6000-idx-studio-1745582025608.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1745582025608.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    The Text needs to be Black in color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,18 +9,18 @@
     <nav className="navbar sticky top-0 bg-background z-10">
       <div className="nav-container">
         <div className="nav-brand">
-          <Link href="/">Philip Clements Samuelraj</Link>
+          <Link href="/" className="dark-charcoal">Philip Clements Samuelraj</Link>
         </div>
         <div className="nav-links">
-          <Link href="/techjays-overview">Techjays Overview</Link>
-          <Link href="/investment-portfolio">Investment Portfolio</Link>
-          <Link href="/media">Media Mentions</Link>
-          <Link href="/hobbies">Hobbies</Link>
-          <Link href="/contact">Contact</Link>
+          <Link href="/techjays-overview" className="dark-charcoal">Techjays Overview</Link>
+          <Link href="/investment-portfolio" className="dark-charcoal">Investment Portfolio</Link>
+          <Link href="/media" className="dark-charcoal">Media Mentions</Link>
+          <Link href="/hobbies" className="dark-charcoal">Hobbies</Link>
+          <Link href="/contact" className="dark-charcoal">Contact</Link>
         </div>
         <div className="social-links">
-          <a href="#"><Linkedin /></a>
-          <a href="#"><Twitter /></a>
+          <a href="#" className="dark-charcoal"><Linkedin /></a>
+          <a href="#" className="dark-charcoal"><Twitter /></a>
         </div>
       </div>
     </nav>
