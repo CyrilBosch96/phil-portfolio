3bc55c3ecf016addf4d41f76a2308e8946f2ebde
Cyril Savari Bosch J P <cyril.jp@techjays.com>
2025-04-28 05:33:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Navbar>
    
    with the text content:
    
        Philip Clements SamuelrajTechjays OverviewInvestment PortfolioMedia MentionsHobbiesContact
    
    Relevant files:
    
        - https://6000-idx-studio-1745582025608.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_1988d3ed._.js
        - https://6000-idx-studio-1745582025608.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1745582025608.cluster-nzwlpk54dvagsxetkvxzbvslyi.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    All text in this bar should be black

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,30 @@
 'use client';
- 
+
  import React from 'react';
  import {Linkedin, Twitter} from 'lucide-react';
  import Link from 'next/link';
- 
+
  const Navbar = () => {
    return (
     <nav className="navbar sticky top-0 bg-background z-10">
       <div className="nav-container">
         <div className="nav-brand">
-          <Link href="/" className="dark-charcoal">Philip Clements Samuelraj</Link>
+           <Link href="/" className="text-black font-semibold">Philip Clements Samuelraj</Link>
         </div>
         <div className="nav-links">
-          <Link href="/techjays-overview" className="dark-charcoal">Techjays Overview</Link>
-          <Link href="/investment-portfolio" className="dark-charcoal">Investment Portfolio</Link>
-          <Link href="/media" className="dark-charcoal">Media Mentions</Link>
-          <Link href="/hobbies" className="dark-charcoal">Hobbies</Link>
-          <Link href="/contact" className="dark-charcoal">Contact</Link>
+           <Link href="/techjays-overview" className="text-black">Techjays Overview</Link>
+           <Link href="/investment-portfolio" className="text-black">Investment Portfolio</Link>
+           <Link href="/media" className="text-black">Media Mentions</Link>
+           <Link href="/hobbies" className="text-black">Hobbies</Link>
+           <Link href="/contact" className="text-black">Contact</Link>
         </div>
         <div className="social-links">
-          <a href="#" className="dark-charcoal"><Linkedin /></a>
-          <a href="#" className="dark-charcoal"><Twitter /></a>
+           <a href="#" className="text-black"><Linkedin /></a>
+           <a href="#" className="text-black"><Twitter /></a>
         </div>
       </div>
     </nav>
   );
 };
- 
+
  export default Navbar;
