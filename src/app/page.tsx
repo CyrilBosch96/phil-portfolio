321e9fb2fdae3f3e7bad3102fd325c1fa8f285ec
Cyril Savari Bosch J P <cyril.jp@techjays.com>
2025-04-28 11:59:16 +0000

    Make sure Philip Clement Samuelraj page are is not scrollable. The other pages must be allowed to scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <>
+    // Added id="home-page-wrapper"
+    <div id="home-page-wrapper" className="flex flex-col h-screen">
       <Navbar />
-      <Hero />
+      <main className="flex-grow">
+        <Hero />
+      </main>
       <Toaster />
-    </>
+    </div>
   );
 }
